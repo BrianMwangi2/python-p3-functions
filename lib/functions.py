78796bd809b64ea04dab6cff2631850ef5845d25
Brian <brian.mwangi2@student.moringaschool.com>
2024-05-22 07:24:26 +0300

    passes

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+name = "Guido"
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+
+
+# Method calls
+greet_programmer()          # Output: Hello, programmer!
+greet(name)                 # Output: Hello, Guido!
+greet_with_default()        # Output: Hello, programmer!
+greet_with_default(name)    # Output: Hello, Guido!
+
 
 def add(num1, num2):
-    pass
+    return num1+num2
+print(add(45,55))
 
 def halve(number):
-    pass
+    return number/2
+print(halve(100))
